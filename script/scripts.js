46ae88467c5517da2d7f5a6e4ddfecf1ccb4aeaf
MonicaHillman <mmhillman1@ucs.br>
2022-12-26 19:37:56 -0300

    criado função de data

--- script/scripts.js
+++ script/scripts.js
@@ -11,11 +11,18 @@ const graficoParaDolar = new Chart(graficoDolar, {
         }]
     },
 });
-
+setInterval(() => conectaAPI(), 5000);
 async function conectaAPI() {
     const conecta = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL");
     const conectaTraduzido = await conecta.json();
     console.log(conectaTraduzido);
 }
 
-conectaAPI();
+function geraHorario() {
+    let data = new Date();
+    let horario = data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds();
+    console.log(horario);
+    return horario;
+}
+
+geraHorario();
