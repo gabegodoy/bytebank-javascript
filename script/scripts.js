ed6d7da8097a0c98b977c4d5fca3ae401fb2c385
MonicaHillman <mmhillman1@ucs.br>
2022-12-27 00:22:01 -0300

    novo grafico

--- script/scripts.js
+++ script/scripts.js
@@ -1,22 +1,17 @@
+import selecionaCotacao from "./imprimeCotacao.js";
 const graficoDolar = document.getElementById('graficoDolar');
 
 const graficoParaDolar = new Chart(graficoDolar, {
     type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: [],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Dólar',
+            data: [],
             borderWidth: 1
         }]
     },
 });
-setInterval(() => conectaAPI(), 5000);
-async function conectaAPI() {
-    const conecta = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL");
-    const conectaTraduzido = await conecta.json();
-    console.log(conectaTraduzido);
-}
 
 function geraHorario() {
     let data = new Date();
@@ -25,4 +20,42 @@ function geraHorario() {
     return horario;
 }
 
-geraHorario();
+function adicionarDados(grafico, legenda, dados) {
+    grafico.data.labels.push(legenda);
+    grafico.data.datasets.forEach((dataset) => {
+        dataset.data.push(dados);
+    })
+    grafico.update();
+}
+
+let workerDolar = new Worker('./script/workers/workerDolar.js');
+workerDolar.postMessage('usd');
+
+workerDolar.addEventListener("message", event => {
+    let tempo = geraHorario();
+    let valor = event.data.ask;
+    selecionaCotacao("dolar", valor);
+    adicionarDados(graficoParaDolar, tempo, valor);
+})
+
+const graficoIene = document.getElementById('graficoIene');
+const graficoParaIene = new Chart(graficoIene, {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            label: 'Iene',
+            data: [],
+            borderWidth: 1
+        }]
+    }
+})
+
+let workerIene = new Worker("./script/workers/workerIene.js");
+workerIene.postMessage("iene");
+workerIene.addEventListener("message", event => {
+    let tempo = geraHorario();
+    let valor = event.data.ask;
+    adicionarDados(graficoParaIene, tempo, valor);
+    selecionaCotacao("iene", valor)
+})
