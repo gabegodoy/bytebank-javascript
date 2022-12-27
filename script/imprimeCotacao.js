56fdcf3db1e585e6fcc12d05ac7482533f8f5925
MonicaHillman <mmhillman1@ucs.br>
2022-12-27 15:07:41 -0300

    implementado webworkers

--- script/imprimeCotacao.js
+++ script/imprimeCotacao.js
@@ -1,24 +1,12 @@
-const lista = document.querySelectorAll('[data-lista]');
+const lista = document.querySelector('[data-lista]');
 
-function selecionaCotacao(nome, valor) {
-    lista.forEach((listaEscolhida) => {
-        if (listaEscolhida.id == nome) {
-            imprimeCotacao(listaEscolhida, nome, valor);
-        }
-    })
-}
-
-function imprimeCotacao(lista, nome, valor) {
+function imprimeCotacao(nome, valor) {
     lista.innerHTML = '';
-    const plurais = {
-        "dolar": "dolares",
-        "iene": "ienes"
-    }
     for (let multiplicador = 1; multiplicador <= 1000; multiplicador *= 10) {
         const listaItem = document.createElement('li');
-        listaItem.innerHTML = `${multiplicador} ${multiplicador == 1 ? nome : plurais[nome]}: R$${(valor * multiplicador).toFixed(2)}`
+        listaItem.innerHTML = `${multiplicador} ${nome}: R$${(valor * multiplicador).toFixed(2)}`
         lista.appendChild(listaItem)
     }
 }
 
-export default selecionaCotacao;
+export default imprimeCotacao;
